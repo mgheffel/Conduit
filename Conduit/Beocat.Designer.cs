95093b6c24faa12d8f58e62aefc842a13abc9550
kbowers <kbowers@OBAMA>
2019-04-18 15:36:34 -0500

    Added Password char for SSH

--- Conduit/Beocat.Designer.cs
+++ Conduit/Beocat.Designer.cs
@@ -75,6 +75,7 @@
             // 
             this.password.Location = new System.Drawing.Point(218, 36);
             this.password.Name = "password";
+            this.password.PasswordChar = '*';
             this.password.Size = new System.Drawing.Size(100, 20);
             this.password.TabIndex = 5;
             // 
