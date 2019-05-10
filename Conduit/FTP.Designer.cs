31812a8a5d876c6a879102a024e8ea1000366235
kbowers <kbowers@OBAMA>
2019-05-10 13:27:32 -0500

    Can now download a specific file to a chosen location

--- Conduit/FTP.Designer.cs
+++ Conduit/FTP.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.fileToDownload = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(189, 23);
+            this.button1.Location = new System.Drawing.Point(21, 22);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +45,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(189, 67);
+            this.button2.Location = new System.Drawing.Point(154, 66);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(161, 23);
             this.button2.TabIndex = 1;
@@ -52,16 +53,25 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // fileToDownload
+            // 
+            this.fileToDownload.Location = new System.Drawing.Point(21, 68);
+            this.fileToDownload.Name = "fileToDownload";
+            this.fileToDownload.Size = new System.Drawing.Size(121, 20);
+            this.fileToDownload.TabIndex = 2;
+            // 
             // FTP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fileToDownload);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "FTP";
             this.Text = "FTP";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +79,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox fileToDownload;
     }
 }
--- Conduit/FTP.cs
+++ Conduit/FTP.cs
@@ -20,24 +20,25 @@ namespace Conduit
     {
         private MainWindow m;
         public FTP(MainWindow v)
-        { 
+        {
             InitializeComponent();
             m = v;
         }
 
-        
-        
+
+
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             string filename = "";
+
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 filename = ofd.FileName;
             }
             //MessageBox.Show(filename);
-                //MessageBox.Show("Create client Object");
-            using (SftpClient sftpClient = new SftpClient(getSftpConnection("headnode.beocat.ksu.edu", m.user, 22,m.password)))
+            //MessageBox.Show("Create client Object");
+            using (SftpClient sftpClient = new SftpClient(getSftpConnection("headnode.beocat.ksu.edu", m.user, 22, m.password)))
             {
                 //MessageBox.Show("Connect to server");
                 sftpClient.Connect();
@@ -62,37 +63,48 @@ namespace Conduit
             /// <summary>
             /// Downloads a file in the desktop synchronously
             /// </summary>
-            
-                
-                string username = m.user;
-                string password = m.password;
 
-                // Path to file on SFTP server
-                string pathRemoteFile = "homes/kbowers/Beocat.txt";
-                // Path where the file should be saved once downloaded (locally)
-                
-                string pathLocalFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Download_Beocat.txt" );
 
-                using (SftpClient sftp = new SftpClient("headnode.beocat.ksu.edu", username, password))
+            string username = m.user;
+            string password = m.password;
+            string locationToWrite = "";
+
+            // Path to file on SFTP server
+            string pathRemoteFile = fileToDownload.Text;//"homes/kbowers/Beocat.txt";
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "TXT Files (*.txt)|*.txt";
+            sfd.DefaultExt = "txt";
+            sfd.AddExtension = true;
+
+
+            sfd.ShowDialog();
+            if (sfd.FileName != null)
+            {
+                locationToWrite = sfd.FileName;
+            }
+            // Path where the file should be saved once downloaded (locally)
+            string pathLocalFile = locationToWrite;//Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Download_Beocat.txt" );
+            fileToDownload.Text = "";
+            using (SftpClient sftp = new SftpClient("headnode.beocat.ksu.edu", username, password))
+            {
+                try
                 {
-                    try
+                    sftp.Connect();
+
+                    MessageBox.Show("Downloading {0}", pathRemoteFile);
+
+                    using (Stream fileStream = File.OpenWrite(pathLocalFile))
                     {
-                        sftp.Connect();
-
-                        Console.WriteLine("Downloading {0}", pathRemoteFile);
-
-                        using (Stream fileStream = File.OpenWrite(pathLocalFile))
-                        {
-                            sftp.DownloadFile(pathRemoteFile, fileStream);
-                        }
-
-                        sftp.Disconnect();
+                        sftp.DownloadFile(pathRemoteFile, fileStream);
                     }
-                    catch (Exception er)
-                    {
-                        Console.WriteLine("An exception has been caught " + er.ToString());
-                    }
+
+                    sftp.Disconnect();
                 }
+                catch (Exception er)
+                {
+                    MessageBox.Show("An exception has been caught " + er.ToString());
+                }
+            }
             MessageBox.Show("File Successfully Downloaded");
         }
     }
